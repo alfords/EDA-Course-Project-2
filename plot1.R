9812757d08a60cc6c5929769e8edd3a2b96479d6
alfords <aaron_zhang@ymail.com>
2014-05-23 12:16:19 +0800

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -16,29 +16,30 @@ if(!file.exists("./data/FNEI.zip")){
 }
 
 # list of files in zip
-# SCCfile  <- "Source_Classification_Code.rds"
+SCCfile  <- "Source_Classification_Code.rds"
 PM25file <- "summarySCC_PM25.rds"
 
 # unzip and caching the result
-# if(!file.exists(paste("./data", SCCfile,  sep="/"))){unzip("./data/FNEI.zip", files=SCCfile,  exdir="./data" )}
+if(!file.exists(paste("./data", SCCfile,  sep="/"))){unzip("./data/FNEI.zip", files=SCCfile,  exdir="./data" )}
 if(!file.exists(paste("./data", PM25file, sep="/"))){unzip("./data/FNEI.zip", files=PM25file, exdir="./data" )}
 
 
-# plotting
+# question 1
 # Have total emissions from PM2.5 decreased in the United States from 1999 to 2008? 
 # Using the base plotting system, make a plot showing the total PM2.5 emission 
 # from all sources for each of the years 1999, 2002, 2005, and 2008.
 
 # read files
-# SCC  <- readRDS(paste("./data", SCCfile,  sep="/"))
+SCC  <- readRDS(paste("./data", SCCfile,  sep="/"))
 NEI  <- readRDS(paste("./data", PM25file, sep="/"))
 
 options(scipen=999)   
 library(plyr)
 em <- ddply(NEI, .(year), summarise, TotalEmissions = sum(Emissions))
 
-# plotting plot
+# plotting 
 png(filename="./figure/plot1.png",  width= 480, height = 480)
 par(mfrow = c(1, 1), bg="transparent")
 barplot(em$TotalEmissions, names.arg=em$year, ylab="Emissions (Tons)", main="Total PM2.5 Emissions")
+
 dev.off()
